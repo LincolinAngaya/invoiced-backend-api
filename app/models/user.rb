0c2a89e47b16eb09a0bc0193ae2008aaa626050a
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-13 23:24:55 +0300

    authentication token working correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
-  include Mongoid::Document
+ 
+  acts_as_token_authenticatable
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   acts_as_token_authenticatable
-   field :authentication_token
+   
+  #  field :authentication_token
+
+
+  
 end
