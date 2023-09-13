4b4fd30b11cf04941f8ac5e4e61c29daf95205f6
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-13 22:19:32 +0300

    correct the create code

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,17 +1,16 @@
 class V1::SessionsController < ApplicationController
 
-    def create 
-        user = User.where(email: params[:email]).first
-
-        if user.valid_password?(params[:password])
-            render json: user.as_json(only: [:id, :email]), status: :created
-
-        else 
-            head(:unauthorized)
-
+    def create
+        user = User.find_by(email: params[:email])
+      
+        if user && user.valid_password?(params[:password])
+          render json: user.as_json(only: [:id, :email]), status: :created
+        else
+          head(:unauthorized)
         end
-    end
-    
+      end
+
+      
     def destroy 
     end
 end
