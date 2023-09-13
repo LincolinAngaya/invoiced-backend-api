e78903b0e22b1471430418542a7706d00b80bbb1
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-13 18:30:23 +0300

    add new contact

--- app/controllers/v1/contact_controller.rb
+++ app/controllers/v1/contact_controller.rb
@@ -1,7 +1,7 @@
-class V1::ContactController < ApplicationController
-    def index 
-        @contacts = Contact.all 
-
-        render json: @contacts, status: :ok
+class V1::ContactsController < ApplicationController
+    def index
+      @contacts = Contact.all
+      render json: @contacts, status: :ok
     end
-end
+  end
+  
