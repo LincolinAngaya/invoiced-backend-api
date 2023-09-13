43a97d8ebe08870f68289049c9ffb58446e30db0
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-13 18:39:00 +0300

    add contact params and create new create end point

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -1,7 +1,21 @@
 class V1::ContactsController < ApplicationController
+
     def index
       @contacts = Contact.all
       render json: @contacts, status: :ok
     end
+
+    def create 
+       @contact = Contact.new(contact_params)
+    end
+
+
+    private 
+
+    def contact_params
+        params.require(:contact).permit(:first_name, :last_name, :email)
+    end
+
   end
+
   
