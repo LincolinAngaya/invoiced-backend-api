03e5b422b52d60005ab5e4ee37c2414fde8a9c76
LincolinAngaya <lincolinangaya5626@gmail.com>
2023-09-13 22:36:11 +0300

    add simpe user authentication user

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class V1::SessionsController < ApplicationController
         user = User.find_by(email: params[:email])
       
         if user && user.valid_password?(params[:password])
-          render json: user.as_json(only: [:id, :email]), status: :created
+          render json: user.as_json(only: [:email, ]), status: :created
         else
           head(:unauthorized)
         end
